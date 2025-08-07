88ccd8753f243b797a6c8ef1c7b0955140ede5c2
duncati <tdelaune@gmail.com>
2025-08-07 21:08:32 +0200

    cpp

--- components/tx_ultimate_touch/tx_ultimate_touch.h
+++ components/tx_ultimate_touch/tx_ultimate_touch.h
@@ -45,6 +45,8 @@ namespace esphome {
             void send_touch_(TouchPoint tp);
             void handle_touch(uint8_t bytes[]);
             TouchPoint get_touch_point(uint8_t bytes[]);
+            uint16_t crc16_modbus(const uint8_t* data, size_t length);
+            int append_crc16_modbus(uint8_t *data, size_t data_len, size_t max_len);
 
             Trigger<TouchPoint> touch_trigger_;
             Trigger<TouchPoint> release_trigger_;
